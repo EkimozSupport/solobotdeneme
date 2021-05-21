fc3be63ef285afd2f0aa01858a640a194e7dc1d2
mamakli06 <83599873+mamakli06@users.noreply.github.com>
2021-05-21 10:48:06 +0300

    Update helpers.js

--- bot/helpers.js
+++ bot/helpers.js
@@ -1,12 +1,13 @@
-const { Composer } = require("grammy");
-const connections = require("../../connections");
-const queues = require("../../queues");
+const config = require("../config");
 
-const composer = new Composer();
+module.exports.link = (msg) => {
+  let chatId = msg.chat.id.toString();
+  if (chatId.startsWith("-100")) chatId = chatId.slice(3, chatId.length);
+  return `https://t.me/c/${chatId}/${msg.reply_to_message.message_id}`;
+};
 
-composer.on(":voice_chat_ended", async (ctx) => {
-  connections.remove(ctx.chat.id);
-  queues.clear(ctx.chat.id);
-});
-
-module.exports = composer;
+module.exports.getFile = async (ctx, fileId) => {
+  return `https://api.telegram.org/file/bot${config.botToken}/${
+    (await ctx.api.getFile(fileId)).file_path
+  }`;
+};
